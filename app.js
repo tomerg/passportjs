c87127c0ff2696efb84dd8e3e4ea3030eb4db6aa
tomerg <tomergoldsmith@Tomers-MacBook-Pro.local>
2016-08-22 14:47:24 +0300

    Added logic

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 var passport = require('passport');
+var bodyParser = require('body-parser');
 
 var LocalStrategy = require('passport-local').Strategy;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
 
 passport.use('login', new LocalStrategy(function (username, password, done) {
   var authenticated = username === "John" && password === "Smith";
