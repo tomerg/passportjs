7cf2874a32756020d8683719799190ebb49b3fef
tomerg <tomergoldsmith@Tomers-MacBook-Pro.local>
2016-08-22 15:00:19 +0300

    Added express sessions

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ var LocalStrategy = require('passport-local').Strategy;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var expressSession = require('express-session');
+app.use(expressSession({ secret: 'mySecretKey' }));
+
 app.use(passport.initialize());
+
+passport.serializeUser(function (user, done) {
+  console.log(user);
+  done(null, user);    // Could store just the id using done(null, user.id);
+});
 
 passport.use('login', new LocalStrategy(function (username, password, done) {
   var authenticated = username === "John" && password === "Smith";
@@ -22,12 +30,11 @@ passport.use('login', new LocalStrategy(function (username, password, done) {
 
 app.post('/login', passport.authenticate('login', {
   successRedirect: '/success',
-  failureRedirect: '/login',
-  session: false
+  failureRedirect: '/login'
 }));
 
 app.get('/success', function (req, res){
-  res.send("Hey, hello from the server!");
+  res.send('Hey, hello from the server!');
 })
 
 app.get('/login', function (req, res) {
